788c8f363a8ded837aa49320de775fbf7772544d
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2023-08-18 13:52:14 +1200

    ENH Update translations

--- client/lang/da.js
+++ client/lang/da.js
@@ -6,15 +6,15 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
   }
 } else {
   ss.i18n.addDictionary('da', {
-    "TOTPRegister.NEXT": "Næste",
     "TOTPRegister.BACK": "Tilbage",
-    "TOTPRegister.INTRO": "Autentificeringskoder bliver genereret af en app på din telefon.",
-    "TOTPRegister.OR": "Eller",
-    "TOTPRegister.MANUAL": "Indtast manuelt den følgende kode ind i autentificerings appen:",
     "TOTPRegister.HOW_TO_USE": "Sådan bruges autentificerings app.",
-    "TOTPVerify.NEXT": "Næste",
+    "TOTPRegister.INTRO": "Autentificeringskoder bliver genereret af en app på din telefon.",
+    "TOTPRegister.MANUAL": "Indtast manuelt den følgende kode ind i autentificerings appen:",
+    "TOTPRegister.NEXT": "Næste",
+    "TOTPRegister.OR": "Eller",
+    "TOTPVerify.ENTER_CODE": "Indtast {length}-cifret kode",
     "TOTPVerify.HOW_TO_USE": "Sådan bruges autentificerings app.",
-    "TOTPVerify.VERIFY": "Brug autentificeringskoden fra din autentificerings app.",
-    "TOTPVerify.ENTER_CODE": "Indtast {length}-cifret kode"
+    "TOTPVerify.NEXT": "Næste",
+    "TOTPVerify.VERIFY": "Brug autentificeringskoden fra din autentificerings app."
 });
 }
